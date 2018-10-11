5f64769da637a6742d4e4bce6c10ecc07dad29f6
Brave_Lion <347964202@qq.com>
2018-10-11 10:55:22 +0800

    Update selpg.go

--- selpg.go
+++ selpg.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"bufio" // 用于从标准输入流获取数据和将数据写入到标准输入流
-	"fmt"	// 用于引用io.EOF 来判断错误是否是文件尾导致
-	"io"	// 用于将错误信息写入到标准错误流
-	"log"	// 用于打开文件和异常退出时发送状态码
-	"os"	// 用于打开文件和异常退出时发送状态码
-	"os/exec"	// 用于开启lp子进程
-	"strings"	// 用于划分、拼接字符串
+	"bufio" // 用于从标准输入流获取数据和将数据写入到标准输入流 //
+	"fmt"	// 用于引用io.EOF 来判断错误是否是文件尾导致 //
+	"io"	// 用于将错误信息写入到标准错误流 //
+	"log"	// 用于打开文件和异常退出时发送状态码 //
+	"os"	// 用于打开文件和异常退出时发送状态码 //
+	"os/exec"	// 用于开启lp子进程 //
+	"strings"	// 用于划分、拼接字符串 //
 
-	flag "github.com/spf13/pflag"	// 用于获取程序运行时用户输入的参数和标识
+	flag "github.com/spf13/pflag"	// 用于获取程序运行时用户输入的参数和标识 //
 )
 
 func main() {
 	//初始化返回的都是对应类型的指针，绑定相应的变量以及赋相应的初始值
 	//定义待解析的命令行参数
-	startNumber := flag.IntP("startpage","s",0,"The page to start printing at [Necessary, no greater than endpage]")
-	endNumber := flag.IntP("endpage","e",0,"The page to end printing at [Necessary, no less than startpage]")
-	lineNumber := flag.IntP("linenumber","1",72,"If this flag is used, a page will consist of a fixed number of characters, which is given by you")
-	forcePage := flag.BoolP("forcepaging","f",false,"Change page only if '-f' appears [Cannot be used with -l]")
+	startNumber := flag.IntP("startpage", "s", 0, "The page to start printing at [Necessary, no greater than endpage]")
+	endNumber := flag.IntP("endpage", "e", 0, "The page to end printing at [Necessary, no less than startpage]")
+	lineNumber := flag.IntP("linenumber", "1", 72, "If this flag is used, a page will consist of a fixed number of characters, which is given by you")
+	forcePage := flag.BoolP("forcepaging", "f", false, "Change page only if '-f' appears [Cannot be used with -l]")
 	destinationPrinter := flag.StringP("destination", "d", "", "Choose a printer to accept the result as a task")
 
 	// StdErr printer //
-	l := log.New(os.Stderr,"",0)
+	l := log.New(os.Stderr, "", 0)
 
 	// Data holder //
 	bytes := make([]byte,65535)
